0faaa246de610d7aae80e2705b1a156520ff67c7
SHAIK ABDUL SHAHANSHA <shaikshaheensha49@gmail.com>
2026-01-24 22:03:48 +0530

    Update image_generator.py

--- image_generator.py
+++ image_generator.py
@@ -1,12 +1,17 @@
 import streamlit as st
 from huggingface_hub import InferenceClient
+import os
 
 client = InferenceClient(
     model="black-forest-labs/FLUX.1-schnell",
-    token=st.secrets["HUGGINGFACE_API_KEY"]
+    token=st.secrets["HF_API_KEY"]
 )
 
 def generate_image(image_prompt, output_path="linkedin_post_image.webp"):
-    image = client.text_to_image(image_prompt)
-    image.save(output_path)
-    return output_path
+    try:
+        image = client.text_to_image(image_prompt)
+        image.save(output_path)
+        return output_path
+    except Exception as e:
+        print("HF IMAGE ERROR:", e)
+        raise RuntimeError(str(e))
